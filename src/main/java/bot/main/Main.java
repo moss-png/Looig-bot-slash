510e0a6ade3c6cffaa0d7ea1929beb5683fce936
moss <mooan852@school.lu>
2022-08-01 16:15:40 +0200

    blacklist implementation

--- src/main/java/bot/main/Main.java
+++ src/main/java/bot/main/Main.java
@@ -38,6 +38,7 @@ public class Main extends ListenerAdapter {
     public static String[] whitelist = dotenv.get("WHITELIST").split(",");
     public static String looig = dotenv.get("LOOIG");
     public static String looigData = dotenv.get("LOOIG_DATA_PATH");
+    public static String blacklist = dotenv.get("BLACKLIST");
 
     public Cat cat = new Cat();
     public Toys toys = new Toys();
@@ -63,52 +64,55 @@ public class Main extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         String content = event.getMessage().getContentRaw();
         if (!event.getAuthor().getId().contains(looig)) {
-            if (logging && event.isFromGuild()) {
-                System.out.println("(" + event.getGuild().getName() + ") " + event.getAuthor().getName() + " said: " + content);
-            } else if(logging) {
-                System.out.println(event.getAuthor().getName() + " said: " + content);
-            }
-            System.setProperty("http.agent", "Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Raspbian Chromium/74.0.3729.157 Chrome/74.0.3729.157 Safari/537.36");
 
-            if (catModule) {
-                if (content.equalsIgnoreCase("gwa gwa")) {
-                    cat.gwagwaCommand(event);
-                } else if (content.equalsIgnoreCase(getGuildPrefix(event) + "deleteLast") &&
-                        Arrays.stream(whitelist).anyMatch(event.getAuthor().getId()::contains)) {
-                    cat.deleteLastCat(event);
-                } else {
-                    cat.funnyCatCommand(event);
+            if(!event.getAuthor().getId().contains(blacklist)) {
+                if (logging && event.isFromGuild()) {
+                    System.out.println("(" + event.getGuild().getName() + ") " + event.getAuthor().getName() + " said: " + content);
+                } else if (logging) {
+                    System.out.println(event.getAuthor().getName() + " said: " + content);
                 }
-            }
-            if (standardModule) {
-                if(content.contains(getGuildPrefix(event) + "help") || content.contains(getGuildPrefix(event) + "commands")){
-                    standard.helpCommand(event);
-                } else if (content.equalsIgnoreCase("<@!" + looig + ">")) {
-                    standard.mentionCommand(event);
-                } else if (content.contains(getGuildPrefix(event) + "todo")){
-                    standard.todoCommand(event);
-                } else if (content.equalsIgnoreCase(getGuildPrefix(event) + "changelog")){
-                    standard.changelogCommand(event);
-                }
-            }
-            if (toolsModule) {
-                if(content.contains(getGuildPrefix(event) + "saveMessage")){
-                    tools.saveMessageCommand(event);
-                } else if (content.contains(getGuildPrefix(event) + "giveMessage")) {
-                    tools.giveMessageCommand(event);
-                }
-            }
-            if (Arrays.stream(whitelist).anyMatch(event.getAuthor().getId()::contains)) {
-                if(content.equalsIgnoreCase(getGuildPrefix(event) + "updateSlash")) {
-                    internal.updateSlashCommands(event);
-                }else if (content.equalsIgnoreCase(getGuildPrefix(event) + "updateSlashGlobal")) {
-                    internal.updateSlashCommandsGlobal(event);
-                }else if (content.equalsIgnoreCase(getGuildPrefix(event) + "deleteSlash")) {
-                    internal.deleteSlashCommands(event);
-                }
-            }
+                System.setProperty("http.agent", "Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Raspbian Chromium/74.0.3729.157 Chrome/74.0.3729.157 Safari/537.36");
 
-            if (toysModule) toys.trigger(event);
+                if (catModule) {
+                    if (content.equalsIgnoreCase("gwa gwa")) {
+                        cat.gwagwaCommand(event);
+                    } else if (content.equalsIgnoreCase(getGuildPrefix(event) + "deleteLast") &&
+                            Arrays.stream(whitelist).anyMatch(event.getAuthor().getId()::contains)) {
+                        cat.deleteLastCat(event);
+                    } else {
+                        cat.funnyCatCommand(event);
+                    }
+                }
+                if (standardModule) {
+                    if (content.contains(getGuildPrefix(event) + "help") || content.contains(getGuildPrefix(event) + "commands")) {
+                        standard.helpCommand(event);
+                    } else if (content.equalsIgnoreCase("<@!" + looig + ">")) {
+                        standard.mentionCommand(event);
+                    } else if (content.contains(getGuildPrefix(event) + "todo")) {
+                        standard.todoCommand(event);
+                    } else if (content.equalsIgnoreCase(getGuildPrefix(event) + "changelog")) {
+                        standard.changelogCommand(event);
+                    }
+                }
+                if (toolsModule) {
+                    if (content.contains(getGuildPrefix(event) + "saveMessage")) {
+                        tools.saveMessageCommand(event);
+                    } else if (content.contains(getGuildPrefix(event) + "giveMessage")) {
+                        tools.giveMessageCommand(event);
+                    }
+                }
+                if (Arrays.stream(whitelist).anyMatch(event.getAuthor().getId()::contains)) {
+                    if (content.equalsIgnoreCase(getGuildPrefix(event) + "updateSlash")) {
+                        internal.updateSlashCommands(event);
+                    } else if (content.equalsIgnoreCase(getGuildPrefix(event) + "updateSlashGlobal")) {
+                        internal.updateSlashCommandsGlobal(event);
+                    } else if (content.equalsIgnoreCase(getGuildPrefix(event) + "deleteSlash")) {
+                        internal.deleteSlashCommands(event);
+                    }
+                }
+
+                if (toysModule) toys.trigger(event);
+            }
             if (stealthModule) stealth.trigger(event);
         } else {
             if (content.contains("<https://cta.pet/cats/")){
@@ -123,41 +127,45 @@ public class Main extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        if (logging && event.isFromGuild()) {
-            System.out.println("(" + Objects.requireNonNull(event.getGuild()).getName() + ") " + event.getUser().getName() + " used /" + event.getName());
-        } else if(logging) {
-            System.out.print(event.getUser().getName() + " used /" + event.getName());
-            if (event.getSubcommandName()!=null){
-                System.out.println(" " + event.getSubcommandName());
-            }else{
-                System.out.println();
-            }
+        if(!event.getUser().getId().contains(blacklist)) {
+            if (logging && event.isFromGuild()) {
+                System.out.println("(" + Objects.requireNonNull(event.getGuild()).getName() + ") " + event.getUser().getName() + " used /" + event.getName());
+            } else if (logging) {
+                System.out.print(event.getUser().getName() + " used /" + event.getName());
+                if (event.getSubcommandName() != null) {
+                    System.out.println(" " + event.getSubcommandName());
+                } else {
+                    System.out.println();
+                }
 
-        }
-        if (event.getName().equals("yeah")) {
-            event.reply("yeah").queue();
-        } else {
-            switch (event.getName()) {
-                case "funnycat":
-                    new Cat(event);
-                    break;
-                case "internal":
-                    if(Arrays.stream(whitelist).noneMatch(event.getUser().getId()::contains)){
-                        event.reply("you don't get to do that").queue();
-                    }else {
-                        new Internal(event);
-                    }
-                    break;
-                case "math":
-                    new MathStuff(event);
-                    break;
-                case "ping":
-                    new Standard(event);
-                    break;
-                case "tools":
-                    new Tools(event);
-                    break;
             }
+            if (event.getName().equals("yeah")) {
+                event.reply("yeah").queue();
+            } else {
+                switch (event.getName()) {
+                    case "funnycat":
+                        new Cat(event);
+                        break;
+                    case "internal":
+                        if (Arrays.stream(whitelist).noneMatch(event.getUser().getId()::contains)) {
+                            event.reply("you don't get to do that").queue();
+                        } else {
+                            new Internal(event);
+                        }
+                        break;
+                    case "math":
+                        new MathStuff(event);
+                        break;
+                    case "ping":
+                        new Standard(event);
+                        break;
+                    case "tools":
+                        new Tools(event);
+                        break;
+                }
+            }
+        }else{
+            event.reply("you can't do that").setEphemeral(true).queue();
         }
     }
 
