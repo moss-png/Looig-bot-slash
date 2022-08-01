a76c2552cb212bedcb46d938ae4606afe9127124
moss <mooan852@school.lu>
2022-08-01 16:06:32 +0200

    cat submissions added

--- src/main/java/bot/main/Main.java
+++ src/main/java/bot/main/Main.java
@@ -10,10 +10,6 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.components.ActionRow;
-import net.dv8tion.jda.api.interactions.components.Modal;
-import net.dv8tion.jda.api.interactions.components.text.TextInput;
-import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
